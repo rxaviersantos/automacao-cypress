8379510cf6145ac2bdae77740fc872ca1a073596
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-04-17 23:21:01 -0300

    :hammer: add script bdd

--- automacao-web/cypress.config.js
+++ automacao-web/cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
+const cucumber = require("cypress-cucumber-preprocessor").default;
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("file:preprocessor", cucumber());
     },
   },
 });
