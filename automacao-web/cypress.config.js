cfdb29376ecadab9c115ac44e7fe06cf87735fd1
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-04-18 20:48:32 -0300

    :tada: add estrutura inicial

---
+++ automacao-web/cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+const cucumber = require("cypress-cucumber-preprocessor").default;
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+
+      on("file:preprocessor", cucumber);
+    },
+  },
+});
