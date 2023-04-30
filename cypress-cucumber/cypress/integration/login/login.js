a5b45bc62648345f50435c8907cb2282bd4333ef
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-04-29 22:24:43 -0300

    :hammer:add script login

---
+++ cypress-cucumber/cypress/integration/login/login.js
@@ -0,0 +1,21 @@
+import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
+
+Given("A user opens the login page", () => {
+  cy.visit("/");
+});
+
+When("A user enter the username {string}", (username) => {
+  cy.get("#user-name").type(username);
+});
+
+And(" A user enter the password {string}", (password) => {
+  cy.get("#password").type(password);
+});
+
+And(" A user clicks on the login button", () => {
+  cy.get("#login-buton").click();
+});
+
+Then("A user will be logged in", () => {
+  cy.url().should("contains", "/inventory.html");
+});
